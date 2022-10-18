cfd0217e68aa65132a8443b1914431baf89c66f3
joozd <euroshopper@gmail.com>
2022-10-18 11:57:14 +0200

    Deprecating

--- app/src/main/java/nl/joozd/joozdter/calendar/CalendarDescriptor.kt
+++
@@ -1,3 +0,0 @@
-package nl.joozd.joozdter.calendar
-
-data class CalendarDescriptor(val calID: Long, val displayName: String, val accountName: String, val ownerName: String, val name: String = "GEEN NAAM", val color: Int=0)
