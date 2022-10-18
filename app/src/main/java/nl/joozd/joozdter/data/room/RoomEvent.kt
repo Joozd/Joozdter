f10ce098d4335f0a960ac4dab7e05e89d9fbc0da
joozd <euroshopper@gmail.com>
2022-10-18 11:54:32 +0200

    RoomEvent now stores much less and has equals function

--- app/src/main/java/nl/joozd/joozdter/data/room/RoomEvent.kt
+++ app/src/main/java/nl/joozd/joozdter/data/room/RoomEvent.kt
@@ -3,32 +3,41 @@ package nl.joozd.joozdter.data.room
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import nl.joozd.joozdter.data.events.Event
-import nl.joozd.joozdter.data.EventTypes
-import java.time.Instant
 
 /**
- * Data class to store an [nl.joozd.joozdter.data.Event]
+ * Data class to keep track of which events are stored.
+ *
+ * An event is primarily stored in the devices calendar.
+ * This class keeps track of what events are stored, so they can be retrieved and deleted/updated.
+ * An event is considered the same if name, start and end times are the same,
+ * so those are the things we store.
+ * @param id: Unique ID for the database, not used otherwise
+ * @param title: The name of this entry, also the text shown in the calendar for this event
+ * @param startTime: Start time of this event (epoch millis)
+ * @param endTime: End time of this event (epoch millis)
  */
 @Entity
 data class RoomEvent(
     @PrimaryKey val id: Long,
-                val name: String,
-                val type: Int,       // EventType.value
-                val startTime: Long, // Instant.epochSecond; no Events without times in DB
-                val endTime: Long,   // Instant.epochSecond; no Events without times in DB
-                val info: String,
-                val notes: String)
-{
-    /**
-     * convert this RoomEvent to a (subclass of) Event
-     */
-    fun toEvent(): Event = Event(
-        name,
-        EventTypes.of(type),
-        Instant.ofEpochSecond(startTime),
-        Instant.ofEpochSecond(endTime),
-        info,
-        notes,
-        id
-    ).withTypeInstance()
+    val title: String,
+    val startTime: Long, // Instant.epochSecond; no Events without times in DB
+    val endTime: Long,   // Instant.epochSecond; no Events without times in DB
+){
+    // When comparing with event, use roomEvent == event, not the other way around!!!
+    override fun equals(other: Any?): Boolean = when(other) {
+        is RoomEvent -> title == other.title
+                && startTime == other.startTime
+                && endTime == other.endTime
+        is Event -> title == other.name
+                && startTime/1000 == other.startEpochMilli
+                && endTime/1000 == other.endEpochMilli
+        else -> false
+    }
+
+    override fun hashCode(): Int {
+        var result = title.hashCode()
+        result = 31 * result + startTime.hashCode()
+        result = 31 * result + endTime.hashCode()
+        return result
+    }
 }
